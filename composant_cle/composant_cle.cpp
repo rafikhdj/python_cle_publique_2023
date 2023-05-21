9996b0ccff633ba3040ff25a40b3f1b59558c813
rafadona <hadjadjrafik@gmail.com>
2023-05-21 23:58:01 +0200

    last version

--- composant_cle/composant_cle.cpp
+++ composant_cle/composant_cle.cpp
@@ -2,6 +2,7 @@
 #include "micro-ecc/uECC.h"
 #include <string>
 #include <sstream>
+#include <iomanip>
 
 char version[]="1.0";
 
@@ -13,24 +14,31 @@ class Cle
 {
     public:
         Cle(){}
-	
-	uint8_t castStringToUint8(const std::string& str) {
-		
-   		std::istringstream iss(str);
-    		int intValue;
-    		iss >> intValue;
-		
-    		if (intValue < 0 || intValue > 255) {}
-    		return static_cast<uint8_t>(intValue);
-	}
-	void initialize(std::string &pk){
-		privatekey = pk;
-		uint8_t a = castStringToUint8(privatekey);
-		uint8_t b = castStringToUint8(publickey);
+
+	void initialize(const std::string &pk){
         const struct uECC_Curve_t * curve = uECC_secp256k1();
-		uECC_Curve curve_256k1 = uECC_secp256k1();
-		uECC_make_key(&a,&b,curve_256k1);
+        uECC_Curve curve_256k1 = uECC_secp256k1();
+
+        uint8_t private_key[uECC_curve_private_key_size(curve)];
+        uint8_t public_key[uECC_curve_public_key_size(curve)];
+
+        uECC_make_key(public_key, private_key, curve_256k1);
+
+        // Convert the keys to hexadecimal string for storage
+        std::stringstream ss;
+        for (int i = 0; i < uECC_curve_private_key_size(curve); i++)
+            ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(private_key[i]);
+
+        privatekey = ss.str();
+
+        ss.str(std::string());  // Clear the stringstream
+
+        for (int i = 0; i < uECC_curve_public_key_size(curve); i++)
+            ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(public_key[i]);
+
+        publickey = ss.str();
 	}
+
         const std::string &getPrivateKey() const {
 	       return privatekey;
        	}
@@ -45,8 +53,7 @@ class Cle
  
 namespace py = pybind11;
 
-
-PYBIND11_MODULE(cle_component,greetings)
+PYBIND11_MODULE(composant_cle, greetings)
 {
   greetings.doc() = "greeting_object 1.0";
   greetings.def("getVersion", &getVersion, "a function returning the version");
@@ -54,7 +61,7 @@ PYBIND11_MODULE(cle_component,greetings)
    // bindings to Cle class
     py::class_<Cle>(greetings, "Cle")
         .def(py::init<>())
-	.def("initialize", &Cle::initialize)
+        .def("initialize", &Cle::initialize, py::arg("pk"))
         .def("getPrivateKey", &Cle::getPrivateKey)
         .def("getPublicKey", &Cle::getPublicKey);
 }
