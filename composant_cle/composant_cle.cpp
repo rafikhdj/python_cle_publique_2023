fc3c4f5663245cdcb7ccbd69aac0f7a1e43193dc
rafadona <hadjadjrafik@gmail.com>
2023-05-21 20:35:34 +0200

    fix bugs

--- composant_cle/composant_cle.cpp
+++ composant_cle/composant_cle.cpp
@@ -1,52 +1,7 @@
 #include <pybind11/pybind11.h>
 #include "micro-ecc/uECC.h"
-
 #include <string>
 #include <sstream>
-#include <vector>
-
-#include <stddef.h>
-
-unsigned char hexchr2bin(const char hex)
-{
-	unsigned char result;
-
-	if (hex >= '0' && hex <= '9') {
-		result = hex - '0';
-	} else if (hex >= 'A' && hex <= 'F') {
-		result = hex - 'A' + 10;
-	} else if (hex >= 'a' && hex <= 'f') {
-		result = hex - 'a' + 10;
-	} else {
-		return 0;
-	}
-	return result;
-}
-
-
-
-void hexStringToBin(unsigned char *out,const char * hexPrivate) {
-    for (int i=0; i<32; i++){
-	out[i] = hexchr2bin(hexPrivate[2*i])<<4 | hexchr2bin(hexPrivate[2*i+1]);
-    }
-}
-
-
-char *binToHexString(char *out,const unsigned char *bin, size_t len)
-{
-    size_t  i;
-
-    if (bin == NULL || len == 0)
-	return NULL;
-
-    for (i=0; i<len; i++) {
-	out[i*2]   = "0123456789abcdef"[bin[i] >> 4];
-	out[i*2+1] = "0123456789abcdef"[bin[i] & 0x0F];
-    }
-    out[len*2] = '\0';
-
-    return out;
-}
 
 char version[]="1.0";
 
@@ -57,14 +12,24 @@ char const* getVersion() {
 class Cle
 {
     public:
-        Cle() {}
-        ~Cle() {}
-
-        void initialize(std::string number){
-            
-            privatekey = hexStringToBin(number);
-            publickey = uECC_compute_public_key(privatekey, publickey, uECC_secp256k1());
-        }
+        Cle(){}
+	
+	uint8_t castStringToUint8(const std::string& str) {
+		
+   		std::istringstream iss(str);
+    		int intValue;
+    		iss >> intValue;
+		
+    		if (intValue < 0 || intValue > 255) {}
+    		return static_cast<uint8_t>(intValue);
+	}
+	void initialize(std::string &pk){
+		privatekey = pk;
+		uint8_t a = castStringToUint8(privatekey);
+		uint8_t b = castStringToUint8(publickey);
+		uECC_Curve curve_256k1 = uECC_secp256k1();
+		uECC_make_key(&a,&b,curve_256k1);
+	}
         const std::string &getPrivateKey() const {
 	       return privatekey;
        	}
@@ -72,8 +37,8 @@ class Cle
            return publickey;
            }
     private:
-        uint8_t privatekey;
-        uint8_t publickey;
+        std::string privatekey;
+        std::string publickey;
         
 };
  
@@ -86,8 +51,9 @@ PYBIND11_MODULE(cle_component,greetings)
   greetings.def("getVersion", &getVersion, "a function returning the version");
   
    // bindings to Cle class
-    py::class_<Cle>(greetings, "Cle")
-        .def("initialize", &Cle::initialize)
+    py::class_<Cle>(greetings, "Cle", py::dynamic_attr())
+        .def(py::init<>())
+	.def("initialize", &Cle::initialize)
         .def("getPrivateKey", &Cle::getPrivateKey)
         .def("getPublicKey", &Cle::getPublicKey);
 }
