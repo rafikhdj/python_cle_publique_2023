f336057b123d2e9fdf33f20921a72bd601584da9
rafadona <hadjadjrafik@gmail.com>
2023-05-21 19:40:21 +0200

    Update composant cle

---
+++ composant_cle/composant_cle.cpp
@@ -0,0 +1,46 @@
+#include <pybind11/pybind11.h>
+#include "micro-ecc/uECC.h"
+
+char version[]="1.0";
+
+char const* getVersion() {
+	return version;
+}
+
+class Cle
+{
+    public:
+        Cle() {}
+        ~Cle() {}
+
+        void initialize(std::string number){
+            
+            privatekey = number;
+            publickey = uECC_compute_public_key(privateKey, publicKey, uECC_secp256k1());
+        }
+        const std::string &getPrivateKey() const {
+	       return privatekey;
+       	}
+        const std::string &getPublicKey() const {
+           return publickey;
+           }
+    private:
+        std::string privatekey;
+        std::string publickey;
+        
+};
+ 
+namespace py = pybind11;
+
+
+PYBIND11_MODULE(cle_component,greetings)
+{
+  greetings.doc() = "greeting_object 1.0";
+  greetings.def("getVersion", &getVersion, "a function returning the version");
+  
+   // bindings to Cle class
+    py::class_<Cle>(greetings, "Cle", py::dynamic_attr())
+        .def(py::init<const std::string &>())
+        .def("getPrivateKey", &Cle::getPrivateKey)
+        .def("getPublicKey", &Cle::getPublicKey)
+}
