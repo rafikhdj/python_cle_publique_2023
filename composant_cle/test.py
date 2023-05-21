acea1e9b78d6a7084a596da1580c28921b20ed76
rafadona <hadjadjrafik@gmail.com>
2023-05-21 19:53:44 +0200

    fix bugs

--- composant_cle/test.py
+++ composant_cle/test.py
@@ -1,6 +1,6 @@
 import composant_cle
 macle = composant_cle.cle()
-macle.initialize(”4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0”)
-macle.getPrivateKey() -> retourne la valeur d’initialisation
-macle.getPublicKey() -> retourne “f2ce1e40befbebaf4045f1a6d126b7b949e7d5adea33f84a09a904093456f4fd504b1f70755be4cef27625b1e6b893e05ffeb361f2971fda1d6be5e730a74303”
+macle.initialize("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0")
+macle.getPrivateKey() 
+macle.getPublicKey() 
 
